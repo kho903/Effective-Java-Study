5e75dad5c51d81a9201a8352151633ee9d4e435f
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-19 18:48:55 +0900

    feat: 완벽 공략 2. 플라이웨이트 패턴 아이템1. 생성자 대신 정적 팩터리 메서드를 고려하라 - 객체를 가볍게 만들어 메모리 사용을 줄이는 패턴. - 자주 변하는 속성(또는 외적인 속성, extrinsit)과 변하지 않는 속성(또는 내적인 속성, intrinsit)을 분리하고 재사용하여 메모리 사용을 줄일 수 있다.

---
+++ effective-java/src/main/java/com/jikim/effectivejava/chap01/item01/flyweight/after/Client.java
@@ -0,0 +1,10 @@
+package com.jikim.effectivejava.chap01.item01.flyweight.after;
+
+public class Client {
+	public static void main(String[] args) {
+		FontFactory fontFactory = new FontFactory();
+		Character c1 = new Character('h', "white", fontFactory.getFont("nanum:12"));
+		Character c2 = new Character('e', "white", fontFactory.getFont("nanum:12"));
+		Character c3 = new Character('l', "white", fontFactory.getFont("nanum:12"));
+	}
+}
---
+++ effective-java/src/main/java/com/jikim/effectivejava/chap01/item01/flyweight/after/Font.java
@@ -0,0 +1,19 @@
+package com.jikim.effectivejava.chap01.item01.flyweight.after;
+
+public class Font {
+	final String family;
+	final int size;
+
+	public Font(String family, int size) {
+		this.family = family;
+		this.size = size;
+	}
+
+	public String getFamily() {
+		return family;
+	}
+
+	public int getSize() {
+		return size;
+	}
+}
