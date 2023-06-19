83519a37b4f0bcd8579571581749adaf264e0f0a
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-19 20:06:50 +0900

    feat: 완벽 공략 5. 리플렉션 아이템1. 생성자 대신 정적 팩터리 메서드를 고려하라 - 클래스로더를 통해 읽어온 클래스 정보를 사용하는 기술 - 리플렉션을 사용해 클래스를 읽어오거나, 인스턴스를 만들거나, 메소드를 실행하거나, 필드의 값을 가져오거나 변경하는 것이 가능. - 언제 사용할까? 	- 특정 애노테이션이 붙어있는 필드 또는 메소드 읽어오기 (JUnit, Spring) 	- 특정 이름 패턴에 해당하는 메소드 목록 가져와 호출하기 (getter, setter)

--- effective-java/src/main/java/com/jikim/effectivejava/chap01/item01/_3/Main.java
+++ effective-java/src/main/java/com/jikim/effectivejava/chap01/item01/_3/Main.java
@@ -1,9 +1,17 @@
 package com.jikim.effectivejava.chap01.item01._3;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class Main {
 
-	/*public static void main(String[] args) {
-		HelloService helloService = HelloServiceFactory.of("ko");
+	public static void main(String[] args) throws
+		ClassNotFoundException,
+		NoSuchMethodException,
+		InvocationTargetException,
+		InstantiationException,
+		IllegalAccessException {
+		/*HelloService helloService = HelloServiceFactory.of("ko");
 		System.out.println("helloService.hello() = " + helloService.hello());
 		helloService = HelloServiceFactory.of("en");
 		System.out.println("helloService.hello() = " + helloService.hello());
@@ -11,6 +19,11 @@ public class Main {
 		HelloService interfaceStaticMethod = HelloService.of("ko");
 		System.out.println("interfaceStaticMethod.hello() = " + interfaceStaticMethod.hello());
 		interfaceStaticMethod = HelloService.of("en");
-		System.out.println("interfaceStaticMethod.hello() = " + interfaceStaticMethod.hello());
-	}*/
+		System.out.println("interfaceStaticMethod.hello() = " + interfaceStaticMethod.hello());*/
+
+		Class<?> aClass = Class.forName("com.jikim.effectivejava.chap01.item01._3.KoreanHelloService");
+		Constructor<?> constructor = aClass.getConstructor();
+		KoreanHelloService o = (KoreanHelloService)constructor.newInstance();
+		System.out.println(o.hello());
+	}
 }
