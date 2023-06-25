57e654166884f353bdea5c8ce13228bd2dab00c4
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-25 18:19:30 +0900

    feat: 객체 프리징 (js), 자바에선 final 아이템 2. 생성자에 매개변수가 많다면 빌더를 고려하라.

---
+++ effective-java/src/main/java/com/jikim/effectivejava/chap01/item02/freeze/FreezeTest.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const jihun = {
+    'name' : 'jihun',
+    'age' : 28
+};
+
+Object.freeze(jihun);
+
+// jihun.name = '김지훈'
+// jihun.kids = ["유담"];
+// jihun.kids.push("유담2");
+
+console.log(jihun.name);
+console.log(jihun.kids);
