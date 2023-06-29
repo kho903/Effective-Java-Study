d60ca2c2be668be632d0c04113ee93644c0c12c5
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-29 15:43:53 +0900

    feat: IllegalArgumentException 아이템 2. 생성자에 매개변수가 많다면 빌더를 고려하라.

---
+++ effective-java/src/main/java/com/jikim/effectivejava/chap01/item02/illegalargumentexception/Order.java
@@ -0,0 +1,15 @@
+package com.jikim.effectivejava.chap01.item02.illegalargumentexception;
+
+import java.time.LocalDate;
+
+public class Order {
+	public void updateDeliveryDate(LocalDate deliveryDate) {
+		if (deliveryDate == null) {
+			throw new NullPointerException("deliveryDate can't be null");
+		}
+		if (deliveryDate.isBefore(LocalDate.now())) {
+			throw new IllegalArgumentException("deliveryDate can't be earlier than " + LocalDate.now());
+		}
+		// 배송 날짜 업데이트
+	}
+}
